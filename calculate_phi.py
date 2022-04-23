16ee0d2ed9c117ed6ade1452fd17ebdc330457f3
Jacqueline Beechert <jbeechert56@gmail.com>
2022-04-23 16:26:02 -0700

    added comments

--- calculate_phi.py
+++ calculate_phi.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def calculate_phi(lon, lat):
 	"""
 	Calculate the Compton scatter angle phi which resulted in the specified
@@ -18,8 +19,12 @@ def calculate_phi(lon, lat):
               Compton scatter angle in degrees.
 	"""
 
+	# These are hard-coded as the z-coordinate of the Compt. scatt.
+	#  and the origin of the diagrams
 	compt_scatt_z = 25
 	origin = [0, 0]
+
+	# Calculate phi from the specified longitude and latitude
 	dist = np.sqrt( (lon - origin[0])**2 + (lat - origin[1])**2 )
 	phi = np.arctan(dist/compt_scatt_z)
 	phi_deg = np.rad2deg(phi)
