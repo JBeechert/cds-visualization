deb73113a5e4347f172c8dabbb4b23165e1ce24d
Jacqueline Beechert <jbeechert56@gmail.com>
2022-04-23 16:02:25 -0700

    remove code folder, restructure the files

---
+++ test.py
@@ -0,0 +1,25 @@
+import numpy as np
+from calculate_phi import calculate_phi
+
+def test_phi_real():
+	phis = []
+	lons = np.arange(-180, 180+1, 1)
+	lats = np.arange(-90, 90+1, 1)
+
+	for lon in lons:
+    		for lat in lats:
+        		phis.append(calculate_phi(lon, lat))
+
+	assert np.isreal(np.array(phis).all())
+
+
+def test_phi_range():
+	phis = []
+	lons = np.arange(-180, 180+1, 1)
+	lats = np.arange(-90, 90+1, 1)
+
+	for lon in lons:
+    		for lat in lats:
+        		phis.append(calculate_phi(lon, lat))
+
+	assert np.abs(np.array(phis).all()) <= 360.
